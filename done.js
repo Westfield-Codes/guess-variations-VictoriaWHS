e94993351820d6a9d03d2b463dba39eec1a860f0
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-09-30 00:25:33 +0000

    5 minutes

--- done.js
+++ done.js
@@ -54,3 +54,23 @@ guessGame();
 }
 // end the function
 }
+
+// create global variable guess, set to 0
+var guess= 0;
+// create a global variable called answer and store a random integer 1-100
+ var answer= Math.floor(Math.random()*100+1);
+// define function guessGame
+function guessGame() {
+// display the answer for testing purposes only
+alert(answer)
+// set guess equal to prompt asking user to "Guess a number, 1-100"
+var guess =prompt("Guess a number 1-100");
+// if guess equals answer display "Correct!" 
+if (guess==answer) alert("correct!");
+// or if guess is greater than answer create a code block
+else if(answer<guess){
+    alert("Nope too high.");
+    guessGame();
+
+
+    
