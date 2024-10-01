a9304f2b3fc15a8fb03ae0cdae3874551177b5df
VictoriaWHS <victoria202975@student-schoolsofwestfield.org>
2024-10-01 16:12:47 +0000

    Done

--- done.js
+++ done.js
@@ -100,3 +100,57 @@ else if (guess>answer) alert("too high");
 }
 // end the function
 }
+
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame(){
+  // generate a random integer 1-100
+let answer= Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(answer);
+  // create guess and set equal to 0 
+var guess= 0;
+  // loop while guess is not equal to answer
+while(guess != answer){ 
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess= prompt("Guess a number,1-100");
+    // if guess equals answer display "Correct!" 
+if (guess == answer) alert("You winnn");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess>answer) alert("nooooo toooo highhhhhhhh");
+    // or if guess is less than answer display "Too low!" 
+else if (guess<answer) alert("Tooooo loowwwwwwwwww.");
+    // else say "Bad input"
+else alert("bad input");
+  // end the loop
+}
+// end the function
+}
+
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame(){
+  // generate a random integer 1-100
+let answer= Math.floor(Math.random()*100+1);
+  // display the integer for testing purposes only
+alert(answer);
+  // create guess and set equal to 0 
+var guess= 0;
+  // loop while guess is not equal to answer
+while(guess != answer){ 
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess= prompt("Guess a number,1-100");
+    // if guess equals answer display "Correct!" 
+if (guess == answer) alert("You winnn");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess>answer) alert("nooooo toooo highhhhhhhh");
+    // or if guess is less than answer display "Too low!" 
+else if (guess<answer) alert("Tooooo loowwwwwwwwww.");
+    // else say "Bad input"
+else alert("bad input");
+  // end the loop
+}
+// end the function
+}
